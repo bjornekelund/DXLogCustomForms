996ece04cdd7daf756f79c25c1cc4a12ab6c1189
Björn Ekelund <bjorn@ekelund.nu>
2019-02-13 05:49:50 +0100

    edits

--- DXLCusForm1/Properties/AssemblyInfo.cs
+++ DXLCusForm1/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DXLCusForm1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 Björn Ekelund SM7IUN")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
